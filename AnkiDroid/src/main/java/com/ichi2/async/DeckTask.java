7b8e1bda86950f5a2d07a0db50025d4bd44ec9d2
Arthur Milchior <Arthur@Milchior.fr>
2019-10-18 02:03:31 +0200

    remove indentations in DeckTask

--- AnkiDroid/src/main/java/com/ichi2/async/DeckTask.java
+++ AnkiDroid/src/main/java/com/ichi2/async/DeckTask.java
@@ -1340,7 +1340,7 @@ public class DeckTask extends BaseAsyncTask<DeckTask.TaskData, DeckTask.TaskData
         Collections.sort(models, new Comparator<JSONObject_>() {
             @Override
             public int compare(JSONObject_ a, JSONObject_ b) {
-                    return a.getString_("name").compareTo(b.getString_("name"));
+                return a.getString_("name").compareTo(b.getString_("name"));
             }
         });
 
