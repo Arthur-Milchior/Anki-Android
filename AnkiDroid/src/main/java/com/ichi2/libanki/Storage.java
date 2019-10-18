ddf59281fb0165b83ebc8d4158ecb3c3e1d253c1
Arthur Milchior <Arthur@Milchior.fr>
2019-10-18 03:34:01 +0200

    Remove indentations in Storage.

--- AnkiDroid/src/main/java/com/ichi2/libanki/Storage.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/Storage.java
@@ -116,148 +116,148 @@ public class Storage {
 
 
     private static void _upgrade(Collection col, int ver) {
-            if (ver < 3) {
-                // new deck properties
-                for (JSONObject_ d : col.getDecks().all()) {
-                    d.put_("dyn", 0);
-                    d.put_("collapsed", false);
-                    col.getDecks().save(d);
+        if (ver < 3) {
+            // new deck properties
+            for (JSONObject_ d : col.getDecks().all()) {
+                d.put_("dyn", 0);
+                d.put_("collapsed", false);
+                col.getDecks().save(d);
+            }
+        }
+        if (ver < 4) {
+            col.modSchemaNoCheck();
+            ArrayList<JSONObject_> clozes = new ArrayList<>();
+            for (JSONObject_ m : col.getModels().all()) {
+                if (!m.getJSONArray_("tmpls").getJSONObject_(0).getString_("qfmt").contains("{{cloze:")) {
+                    m.put_("type", Consts.MODEL_STD);
+                } else {
+                    clozes.add(m);
                 }
             }
-            if (ver < 4) {
-                col.modSchemaNoCheck();
-                ArrayList<JSONObject_> clozes = new ArrayList<>();
-                for (JSONObject_ m : col.getModels().all()) {
-                    if (!m.getJSONArray_("tmpls").getJSONObject_(0).getString_("qfmt").contains("{{cloze:")) {
-                        m.put_("type", Consts.MODEL_STD);
-                    } else {
-                        clozes.add(m);
+            for (JSONObject_ m : clozes) {
+                try {
+                    _upgradeClozeModel(col, m);
+                } catch (ConfirmModSchemaException e) {
+                    // Will never be reached as we already set modSchemaNoCheck()
+                    throw new RuntimeException(e);
+                }
+            }
+            col.getDb().execute("UPDATE col SET ver = 4");
+        }
+        if (ver < 5) {
+            col.getDb().execute("UPDATE cards SET odue = 0 WHERE queue = 2");
+            col.getDb().execute("UPDATE col SET ver = 5");
+        }
+        if (ver < 6) {
+            col.modSchemaNoCheck();
+            for (JSONObject_ m : col.getModels().all()) {
+                m.put_("css", new JSONObject_(Models.defaultModel).getString_("css"));
+                JSONArray_ ar = m.getJSONArray_("tmpls");
+                for (int i = 0; i < ar.length(); i++) {
+                    JSONObject_ t = ar.getJSONObject_(i);
+                    if (!t.has("css")) {
+                        continue;
+                    }
+                    m.put_("css",
+                            m.getString_("css") + "\n"
+                                    + t.getString_("css").replace(".card ", ".card" + t.getInt_("ord") + 1));
+                    t.remove("css");
+                }
+                col.getModels().save(m);
+            }
+            col.getDb().execute("UPDATE col SET ver = 6");
+        }
+        if (ver < 7) {
+            col.modSchemaNoCheck();
+            col.getDb().execute("UPDATE cards SET odue = 0 WHERE (type = 1 OR queue = 2) AND NOT odid");
+            col.getDb().execute("UPDATE col SET ver = 7");
+        }
+        if (ver < 8) {
+            col.modSchemaNoCheck();
+            col.getDb().execute("UPDATE cards SET due = due / 1000 WHERE due > 4294967296");
+            col.getDb().execute("UPDATE col SET ver = 8");
+        }
+        if (ver < 9) {
+            col.getDb().execute("UPDATE col SET ver = 9");
+        }
+        if (ver < 10) {
+            col.getDb().execute("UPDATE cards SET left = left + left * 1000 WHERE queue = 1");
+            col.getDb().execute("UPDATE col SET ver = 10");
+        }
+        if (ver < 11) {
+            col.modSchemaNoCheck();
+            for (JSONObject_ d : col.getDecks().all()) {
+                if (d.getInt_("dyn") != 0) {
+                    int order = d.getInt_("order");
+                    // failed order was removed
+                    if (order >= 5) {
+                        order -= 1;
+                    }
+                    JSONArray_ ja = new JSONArray_(Arrays.asList(new Object[] { d.getString_("search"),
+                            d.getInt_("limit"), order }));
+                    d.put_("terms", new JSONArray_());
+                    d.getJSONArray_("terms").put_(0, ja);
+                    d.remove("search");
+                    d.remove("limit");
+                    d.remove("order");
+                    d.put_("resched", true);
+                    d.put_("return", true);
+                } else {
+                    if (!d.has("extendNew")) {
+                        d.put_("extendNew", 10);
+                        d.put_("extendRev", 50);
                     }
                 }
-                for (JSONObject_ m : clozes) {
-                    try {
-                        _upgradeClozeModel(col, m);
-                    } catch (ConfirmModSchemaException e) {
-                        // Will never be reached as we already set modSchemaNoCheck()
-                        throw new RuntimeException(e);
-                    }
+                col.getDecks().save(d);
+            }
+            for (JSONObject_ c : col.getDecks().allConf()) {
+                JSONObject_ r = c.getJSONObject_("rev");
+                r.put_("ivlFct", r.optDouble("ivlFct", 1));
+                if (r.has("ivlfct")) {
+                    r.remove("ivlfct");
                 }
-                col.getDb().execute("UPDATE col SET ver = 4");
+                r.put_("maxIvl", 36500);
+                col.getDecks().save(c);
             }
-            if (ver < 5) {
-                col.getDb().execute("UPDATE cards SET odue = 0 WHERE queue = 2");
-                col.getDb().execute("UPDATE col SET ver = 5");
-            }
-            if (ver < 6) {
-                col.modSchemaNoCheck();
-                for (JSONObject_ m : col.getModels().all()) {
-                    m.put_("css", new JSONObject_(Models.defaultModel).getString_("css"));
-                    JSONArray_ ar = m.getJSONArray_("tmpls");
-                    for (int i = 0; i < ar.length(); i++) {
-                        JSONObject_ t = ar.getJSONObject_(i);
-                        if (!t.has("css")) {
-                            continue;
-                        }
-                        m.put_("css",
-                                m.getString_("css") + "\n"
-                                        + t.getString_("css").replace(".card ", ".card" + t.getInt_("ord") + 1));
-                        t.remove("css");
-                    }
-                    col.getModels().save(m);
+            for (JSONObject_ m : col.getModels().all()) {
+                JSONArray_ tmpls = m.getJSONArray_("tmpls");
+                for (int ti = 0; ti < tmpls.length(); ++ti) {
+                    JSONObject_ t = tmpls.getJSONObject_(ti);
+                    t.put_("bqfmt", "");
+                    t.put_("bafmt", "");
                 }
-                col.getDb().execute("UPDATE col SET ver = 6");
+                col.getModels().save(m);
             }
-            if (ver < 7) {
-                col.modSchemaNoCheck();
-                col.getDb().execute("UPDATE cards SET odue = 0 WHERE (type = 1 OR queue = 2) AND NOT odid");
-                col.getDb().execute("UPDATE col SET ver = 7");
-            }
-            if (ver < 8) {
-                col.modSchemaNoCheck();
-                col.getDb().execute("UPDATE cards SET due = due / 1000 WHERE due > 4294967296");
-                col.getDb().execute("UPDATE col SET ver = 8");
-            }
-            if (ver < 9) {
-                col.getDb().execute("UPDATE col SET ver = 9");
-            }
-            if (ver < 10) {
-                col.getDb().execute("UPDATE cards SET left = left + left * 1000 WHERE queue = 1");
-                col.getDb().execute("UPDATE col SET ver = 10");
-            }
-            if (ver < 11) {
-                col.modSchemaNoCheck();
-                for (JSONObject_ d : col.getDecks().all()) {
-                    if (d.getInt_("dyn") != 0) {
-                        int order = d.getInt_("order");
-                        // failed order was removed
-                        if (order >= 5) {
-                            order -= 1;
-                        }
-                        JSONArray_ ja = new JSONArray_(Arrays.asList(new Object[] { d.getString_("search"),
-                                d.getInt_("limit"), order }));
-                        d.put_("terms", new JSONArray_());
-                        d.getJSONArray_("terms").put_(0, ja);
-                        d.remove("search");
-                        d.remove("limit");
-                        d.remove("order");
-                        d.put_("resched", true);
-                        d.put_("return", true);
-                    } else {
-                        if (!d.has("extendNew")) {
-                            d.put_("extendNew", 10);
-                            d.put_("extendRev", 50);
-                        }
-                    }
-                    col.getDecks().save(d);
-                }
-                for (JSONObject_ c : col.getDecks().allConf()) {
-                    JSONObject_ r = c.getJSONObject_("rev");
-                    r.put_("ivlFct", r.optDouble("ivlFct", 1));
-                    if (r.has("ivlfct")) {
-                        r.remove("ivlfct");
-                    }
-                    r.put_("maxIvl", 36500);
-                    col.getDecks().save(c);
-                }
-                for (JSONObject_ m : col.getModels().all()) {
-                    JSONArray_ tmpls = m.getJSONArray_("tmpls");
-                    for (int ti = 0; ti < tmpls.length(); ++ti) {
-                        JSONObject_ t = tmpls.getJSONObject_(ti);
-                        t.put_("bqfmt", "");
-                        t.put_("bafmt", "");
-                    }
-                    col.getModels().save(m);
-                }
-                col.getDb().execute("update col set ver = 11");
-            }
+            col.getDb().execute("update col set ver = 11");
+        }
     }
 
 
     private static void _upgradeClozeModel(Collection col, JSONObject_ m) throws ConfirmModSchemaException {
-            m.put_("type", Consts.MODEL_CLOZE);
-            // convert first template
-            JSONObject_ t = m.getJSONArray_("tmpls").getJSONObject_(0);
-            for (String type : new String[] { "qfmt", "afmt" }) {
-                t.put_(type, t.getString_(type).replaceAll("\\{\\{cloze:1:(.+?)\\}\\}", "{{cloze:$1}}"));
+        m.put_("type", Consts.MODEL_CLOZE);
+        // convert first template
+        JSONObject_ t = m.getJSONArray_("tmpls").getJSONObject_(0);
+        for (String type : new String[] { "qfmt", "afmt" }) {
+            t.put_(type, t.getString_(type).replaceAll("\\{\\{cloze:1:(.+?)\\}\\}", "{{cloze:$1}}"));
+        }
+        t.put_("name", "Cloze");
+        // delete non-cloze cards for the model
+        JSONArray_ ja = m.getJSONArray_("tmpls");
+        ArrayList<JSONObject_> rem = new ArrayList<>();
+        for (int i = 1; i < ja.length(); i++) {
+            JSONObject_ ta = ja.getJSONObject_(i);
+            if (!ta.getString_("afmt").contains("{{cloze:")) {
+                rem.add(ta);
             }
-            t.put_("name", "Cloze");
-            // delete non-cloze cards for the model
-            JSONArray_ ja = m.getJSONArray_("tmpls");
-            ArrayList<JSONObject_> rem = new ArrayList<>();
-            for (int i = 1; i < ja.length(); i++) {
-                JSONObject_ ta = ja.getJSONObject_(i);
-                if (!ta.getString_("afmt").contains("{{cloze:")) {
-                    rem.add(ta);
-                }
-            }
-            for (JSONObject_ r : rem) {
-                col.getModels().remTemplate(m, r);
-            }
-            JSONArray_ newArray = new JSONArray_();
-            newArray.put(ja.get_(0));
-            m.put_("tmpls", newArray);
-            col.getModels()._updateTemplOrds(m);
-            col.getModels().save(m);
+        }
+        for (JSONObject_ r : rem) {
+            col.getModels().remTemplate(m, r);
+        }
+        JSONArray_ newArray = new JSONArray_();
+        newArray.put(ja.get_(0));
+        m.put_("tmpls", newArray);
+        col.getModels()._updateTemplOrds(m);
+        col.getModels().save(m);
 
     }
 
@@ -319,22 +319,22 @@ public class Storage {
 
 
     private static void _setColVars(DB db) {
-            JSONObject_ g = new JSONObject_(Decks.defaultDeck);
-            g.put_("id", 1);
-            g.put_("name", "Default");
-            g.put_("conf", 1);
-            g.put_("mod", Utils.intTime());
-            JSONObject_ gc = new JSONObject_(Decks.defaultConf);
-            gc.put_("id", 1);
-            JSONObject_ ag = new JSONObject_();
-            ag.put_("1", g);
-            JSONObject_ agc = new JSONObject_();
-            agc.put_("1", gc);
-            ContentValues values = new ContentValues();
-            values.put("conf", Collection.defaultConf);
-            values.put("decks", Utils.jsonToString(ag));
-            values.put("dconf", Utils.jsonToString(agc));
-            db.update("col", values);
+        JSONObject_ g = new JSONObject_(Decks.defaultDeck);
+        g.put_("id", 1);
+        g.put_("name", "Default");
+        g.put_("conf", 1);
+        g.put_("mod", Utils.intTime());
+        JSONObject_ gc = new JSONObject_(Decks.defaultConf);
+        gc.put_("id", 1);
+        JSONObject_ ag = new JSONObject_();
+        ag.put_("1", g);
+        JSONObject_ agc = new JSONObject_();
+        agc.put_("1", gc);
+        ContentValues values = new ContentValues();
+        values.put("conf", Collection.defaultConf);
+        values.put("decks", Utils.jsonToString(ag));
+        values.put("dconf", Utils.jsonToString(agc));
+        db.update("col", values);
     }
 
 
