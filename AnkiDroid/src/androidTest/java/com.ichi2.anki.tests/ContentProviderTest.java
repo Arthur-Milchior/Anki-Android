0491c8bc166e0ce786029a50b09c5b825a3fe723
Arthur Milchior <Arthur@Milchior.fr>
2019-10-26 02:07:19 +0200

    Use modSchemaNoCheck instead of modSchema(false)
    This allows to avoid
    ```
                } catch (ConfirmModSchemaException e) {
                    //This should never be reached because modSchema() didn't throw an exception
                }
    ```

--- AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/ContentProviderTest.java
+++ AnkiDroid/src/androidTest/java/com.ichi2.anki.tests/ContentProviderTest.java
@@ -160,7 +160,7 @@ public class ContentProviderTest {
         col.getDecks().flush();
         assertEquals("Check that all created decks have been deleted", mNumDecksBeforeTest, col.getDecks().count());
         // Delete test model
-        col.modSchema(false);
+        col.modSchemaNoCheck();
 
         removeAllModelsByName(col, BASIC_MODEL_NAME);
         removeAllModelsByName(col, TEST_MODEL_NAME);
@@ -454,7 +454,7 @@ public class ContentProviderTest {
         } finally {
             // Delete the model (this will force a full-sync)
             try {
-                col.modSchema(false);
+                col.modSchemaNoCheck();
                 col.getModels().rem(col.getModels().get(mid));
             } catch (ConfirmModSchemaException e) {
                 // This will never happen
