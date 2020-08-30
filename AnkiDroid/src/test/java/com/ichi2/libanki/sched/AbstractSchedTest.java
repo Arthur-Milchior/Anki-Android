d123251dfb85927f09458d688319750b953fd779
Arthur Milchior <Arthur@Milchior.fr>
2020-08-30 22:09:37 +0200

    NF: remove wait for task

--- AnkiDroid/src/test/java/com/ichi2/libanki/sched/AbstractSchedTest.java
+++ AnkiDroid/src/test/java/com/ichi2/libanki/sched/AbstractSchedTest.java
@@ -100,7 +100,7 @@ public class AbstractSchedTest extends RobolectricTest {
 
         sched.answerCard(cardBeforeUndo, EASE_3);
 
-        waitForTask(UNDO, 5000);
+        getCol().getTaskManager().launchCollectionTask(UNDO);
 
         int[] countsAfterUndo = sched.counts();
 
