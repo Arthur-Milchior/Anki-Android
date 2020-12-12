e24e0d4bf6b0b8fa637e209d46202108f8deb37b
Arthur Milchior <Arthur@Milchior.fr>
2020-12-12 07:04:10 +0100

    NF: `download` return ConnectionResultType instead of pair
    The pair was useless as second element was always null

--- AnkiDroid/src/main/java/com/ichi2/libanki/sync/HttpSyncer.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/sync/HttpSyncer.java
@@ -377,7 +377,7 @@ public class HttpSyncer {
     }
 
 
-    public Pair<Syncer.ConnectionResultType, Object[]> download() throws UnknownHttpResponseException {
+    public Syncer.ConnectionResultType download() throws UnknownHttpResponseException {
         return null;
     }
 
