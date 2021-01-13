0ca54af1ae66e3de93fef9a303db3b5d9c3d2ca3
Arthur Milchior <Arthur@Milchior.fr>
2021-01-13 22:51:07 +0100

    NF: Allow tests to execute everything in foreground

--- AnkiDroid/src/test/java/com/ichi2/async/ForegroundTaskManager.java
+++ AnkiDroid/src/test/java/com/ichi2/async/ForegroundTaskManager.java
@@ -1,6 +1,7 @@
 package com.ichi2.async;
 
 import com.ichi2.libanki.CollectionGetter;
+import com.ichi2.libanki.Collection;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,15 +30,14 @@ public class ForegroundTaskManager extends TaskManager {
     public <ProgressListener, ProgressBackground extends ProgressListener, ResultListener, ResultBackground extends ResultListener> CollectionTask<ProgressListener, ProgressBackground, ResultListener, ResultBackground> launchCollectionTask(
             @NonNull CollectionTask.Task<ProgressBackground, ResultBackground> task,
             @Nullable TaskListener<ProgressListener, ResultListener> listener) {
-        CollectionTask<ProgressListener, ProgressBackground, ResultListener, ResultBackground>  ct =
-                new CollectionTask<>(task, listener, null);
-        ct.execute();
-        try {
-            ct.wait();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        if (listener != null) {
+            listener.onPreExecute();
         }
-        return ct;
+        ResultBackground res = task.task(mColGetter.getCol(), new MockTaskManager<>(listener));
+        if (listener != null) {
+            listener.onPostExecute(res);
+        }
+        return new EmptyTask<>(task, listener);
     }
 
 
@@ -65,5 +65,35 @@ public class ForegroundTaskManager extends TaskManager {
     @Override
     public boolean waitForAllToFinish(Integer timeoutSeconds) {
         return true;
+    }
+
+    public class MockTaskManager<ProgressListener, ProgressBackground extends ProgressListener> implements ProgressSenderAndCancelListener<ProgressBackground> {
+
+        private final @Nullable TaskListener<ProgressListener, ?> mTaskListener;
+
+
+        public MockTaskManager(@Nullable TaskListener<ProgressListener, ?> listener) {
+            mTaskListener = listener;
+        }
+
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
+
+
+        @Override
+        public void doProgress(@Nullable ProgressBackground value) {
+            mTaskListener.onProgressUpdate(value);
+        }
+    }
+
+    public class EmptyTask<ProgressListener, ProgressBackground extends ProgressListener, ResultListener, ResultBackground extends ResultListener> extends
+            CollectionTask<ProgressListener, ProgressBackground, ResultListener, ResultBackground> {
+
+        protected EmptyTask(Task<ProgressBackground, ResultBackground> task, TaskListener<ProgressListener, ResultListener> listener) {
+            super(task, listener, null);
+        }
     }
 }
